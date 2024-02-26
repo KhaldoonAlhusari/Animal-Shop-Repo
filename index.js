e84be5fc9f7b67c0b201986bce0a34239b683d71
KhaldoonAlhusari <khaldoonalhusari@gmail.com>
2024-02-26 15:40:03 +0400

    Fixed issues for deployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config({ path: '.env'});
+require('dotenv').config({ path: '.env'});
 const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
